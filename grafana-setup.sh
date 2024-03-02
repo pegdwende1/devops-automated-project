e3bbe01aec67ef3ac8af65d00ebc78e8771263cc
pegdwende1 <136675538+pegdwende1@users.noreply.github.com>
2024-03-01 21:05:50 -0600

    Update grafana-setup.sh

--- grafana-setup.sh
+++ grafana-setup.sh
@@ -17,7 +17,7 @@ sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_export
 rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
 
 # setup the node-exporter dependencies
-sudo git clone -b installations https://github.com/cvamsikrishna11/devops-fully-automated.git /tmp/devops-fully-automated
+sudo git clone -b setup https://github.com/pegdwende1/devops-automated-project.git /tmp/devops-automated-project
 sudo cp /tmp/devops-fully-automated/prometheus-setup-dependencies/node-exporter.service /etc/systemd/system/node-exporter.service
 
 sudo systemctl daemon-reload
